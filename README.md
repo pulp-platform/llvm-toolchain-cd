288590cbd250630ef64fe43d8090b7ea013d365e
Noah Huetter <noahhuetter@gmail.com>
2021-06-18 10:42:41 +0200

    add release scripts and documentation

--- README.md
+++ README.md
@@ -8,3 +8,28 @@ Continuous delivery of the Snitch LLVM toolchain.
 |------|----|------|--------|
 | riscv32-snitch-llvm-centos7.tar.gz | CentOS 7 | [link](https://sourceforge.net/projects/snitch-llvm/files/nightly/riscv32-snitch-llvm-centos7.tar.gz/download) | [![riscv32-llvm-centos7](https://github.com/pulp-platform/snitch-toolchain-cd/actions/workflows/riscv32-llvm-centos7.yml/badge.svg)](https://github.com/pulp-platform/snitch-toolchain-cd/actions/workflows/riscv32-llvm-centos7.yml) |
 | riscv32-snitch-llvm-ubuntu2004.tar.gz | Ubuntu 20.04 | [link](https://sourceforge.net/projects/snitch-llvm/files/nightly/riscv32-snitch-llvm-ubuntu2004.tar.gz/download) | [![riscv32-llvm-ubuntu2004](https://github.com/pulp-platform/snitch-toolchain-cd/actions/workflows/riscv32-llvm-ubuntu2004.yml/badge.svg)](https://github.com/pulp-platform/snitch-toolchain-cd/actions/workflows/riscv32-llvm-ubuntu2004.yml) |
+
+## Tagging a new release
+
+Naming scheme: `12.0.0-snitch-0.1.0-rc1`
+
+- Create a new tag in [snitch-llvm][snitch-llvm]
+- Build for all releases using `stages/release.sh`
+- Upload artifacts to the release
+- Create release notes
+```bash
+last_tag=12.0.0-snitch-0.1.0-rc1
+new_tag=12.0.0-snitch-0.1.0-rc2
+echo "## Changes since last release"
+echo "Last release: \`$last_tag\`"
+git --no-pager log $last_tag..$new_tag --format="- %C(auto) %h %s"
+```
+
+- Trigger a new run of `build-docker` in [snitch][snitch] so that the docker container contains newest release or manually in the `snitch` repo
+```bash
+docker build -t snitch -f util/container/Dockerfile .
+docker tag snitch ghcr.io/pulp-platform/snitch:0.1.0-rc1
+```
+
+[snitch-llvm]: https://github.com/pulp-platform/snitch-llvm
+[snitch]: https://github.com/pulp-platform/snitch
---
+++ stages/release.sh
@@ -0,0 +1,69 @@
+#!/bin/bash -ex
+# Copyright 2020 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+VERSION=snitch-0.1.0-rc2
+
+WORKDIR=${PWD}
+OUTDIR=${WORKDIR}/.release
+PARALLEL_JOBS=$(expr $(nproc) - 1)
+
+# Specify versions to use
+NEWLIB_VERSION=3.3.0
+LLVM_VERSION=12.0.0-${VERSION}
+
+# Build variables
+BUGURL=https://github.com/pulp-platform/snitch-llvm/issues
+TRIPLE=riscv32-snitch-llvm
+# LLVM_VERSION_SUFFIX is set to "-${BUILDNO}"
+BUILDNO=${VERSION}
+
+# All OSes to build
+OSes="ubuntu1804" # ubuntu1804 ubuntu2004 centos7
+
+# prepare
+mkdir -p ${OUTDIR}
+echo "---------------------------" >> ${OUTDIR}/buildlog.txt
+echo "VERSION       $VERSION" >> ${OUTDIR}/buildlog.txt
+echo "OSes          $OSes" >> ${OUTDIR}/buildlog.txt
+echo "LLVM_VERSION  $LLVM_VERSION" >> ${OUTDIR}/buildlog.txt
+
+# For each OS, build the toolchain
+for os in $OSes; do
+  # Temp dir
+  tmpdir=$(mktemp -d); cd ${tmpdir}
+
+  # shallow-clone newlib and llvm
+  git clone --depth 1 -b newlib-${NEWLIB_VERSION} https://sourceware.org/git/newlib-cygwin.git newlib
+  git clone --depth 1 -b ${LLVM_VERSION} https://github.com/pulp-platform/snitch-llvm.git llvm-project
+
+  # Build builder container
+  docker build -t linux-${os}:latest -f ${WORKDIR}/docker/linux-${os}.Dockerfile .
+
+  # Copy compile script to working directory
+  cp -r ${WORKDIR}/stages/build-riscv32-llvm.sh .
+
+  # Compile toolchain
+  PKGVERS=${TRIPLE}-${os}-${VERSION}
+  docker run -v $PWD:/home/builder -w/home/builder \
+              -e BUGURL=${BUGURL} \
+              -e PKGVERS=${PKGVERS} \
+              -e BUILDNO=${BUILDNO} \
+              -e PARALLEL_JOBS=${PARALLEL_JOBS} \
+              linux-${os}:latest \
+              ./build-riscv32-llvm.sh
+
+  # Check version
+  echo ">>> Version for OS $os" >> ${OUTDIR}/buildlog.txt
+  PKGVERS=${TRIPLE}-${os}-${VERSION}
+  docker run -v $PWD:/home/builder -w/home/builder \
+              linux-${os}:latest \
+              /bin/bash -c "install/bin/clang --version; install/bin/llvm-config --version" >> ${OUTDIR}/buildlog.txt
+
+  # Package
+  tar -czf ${OUTDIR}/${PKGVERS}.tar.gz --transform s/^install/${PKGVERS}/ install
+
+  # exit and cleanup
+  docker run -v $PWD:/home/builder -w/home/builder linux-${os}:latest /bin/bash -c "rm -rf build install"
+done
